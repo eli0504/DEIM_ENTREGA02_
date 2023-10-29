b0e010b62ac4fa96a16e9e3f265ad48ed0337cc7
eli0504 <2003ecc@gmail.com>
2023-10-29 12:10:57 +0100

    revision

--- Assets/Scripts/Tokens.cs
+++ Assets/Scripts/Tokens.cs
@@ -4,44 +4,52 @@ using UnityEngine;
 
 public class Tokens : MonoBehaviour
 {
-    private Chessboard chessboard;
+    private Chessboard chessboard; // for setup
 
     private Transform pos;
-    public Vector3[] tokensPos; //positions array
+    public Vector3[] tokensPos = new Vector3[35]; //positions array
     
 
     public GameObject tokensPrefab;
-    public int tokensIndex;
+    private int tokensIndex;
 
-    public float gridMoveTimer = 3;
-    public float gridMoveTimerMax = 5f;
+    private float gridMoveTimer;
+    private float gridMoveTimerMax = 3f; //the token appears every 3 seconds
 
-    private void Start()
+
+    private void Update()
+    {
+        SpawnTokens();
+        OccupiedPositions();
+    }
+
+    private void SpawnTokens()
     {
         if (gridMoveTimer >= gridMoveTimerMax)
         {
             gridMoveTimer -= gridMoveTimerMax;
         }
-    }
-    private void Update()
-    {
-        SpawnTokens();
-    }
 
-    private void SpawnTokens()
-    {
+        //the token appears in random positions
         float posx = Random.Range(-3, 3);
         float posy = Random.Range(-2, 2);
         Vector2 randomPos = new Vector2(posx, posy);
-
-        //for (int i = 0; i < tokensPos.Length; i++)
-        //{
-            int tokensIndex = Random.Range(0, tokensPos.Length);
-            Instantiate(tokensPrefab, tokensPos[tokensIndex], tokensPrefab.transform.rotation);
-            
-        //}
+          
+        tokensIndex = Random.Range(0, tokensPos.Length);
+        Instantiate(tokensPrefab, tokensPos[tokensIndex], tokensPrefab.transform.rotation);
     }
 
+    private void OccupiedPositions() //stop time when all positions are occupied
+    {
+        if(tokensIndex >= tokensPos.Length)
+        {
+            Time.timeScale = 0; 
+        }
+        else
+        {
+            SpawnTokens();
+        }
+    }
 
     //Scripts connection
     public void Setup(Chessboard chessboard)  //public function that needs info of the script Chessboard
